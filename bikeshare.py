399918c0b8b09a9586a834189e13b4ffaf6972ba
Sergiu Conovalu <sergiu.conovalu@gmail.com>
2026-04-15 11:40:07 +0300

    Calculate stitistics comment added

--- bikeshare.py
+++ bikeshare.py
@@ -196,12 +196,12 @@ def main():
     while True:
         city, month, day = get_filters()
         df = load_data(city, month, day)
-
+# Calculate statistics 
         time_stats(df)
         station_stats(df)
         trip_duration_stats(df)
         user_stats(df)
-
+# Show raw data
         show_data = input('\nWould you like to see 5 lines of raw data? Enter yes or no.\n').lower()
         start_loc = 0
     
